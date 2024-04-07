56d5d74938b41965726c9460f5218fa5eab93166
Gloria Jumai Williams <willyabiodun@gmail.com>
2024-04-07 19:51:29 +0100

    first part of the assignement done

--- assignment3.js
+++ assignment3.js
@@ -5,3 +5,11 @@ let programming = {
     difficulty: 8,
     jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
 };
+
+
+    programming.languages.push("Go"); //wrote the command to add the language "Go" to the end of the languages array
+    programming.difficulty = 7; //Changed the difficulty to the value of 7
+    delete programming.jokes; // Wrote the command to remove the jokes from the prgramming object
+    programming.isFun = true; // Wrote the command to add a new key called isFun and a value of true t the programming object
+
+    console.log(programming);
